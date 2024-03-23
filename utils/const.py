0dad3e2b83b1caeabf45a5099c839fefbf163d2f
aboitreaud <aboitreaud@gmail.com>
2024-03-23 17:10:27 +0800

    Modify prompt
    Adjust instructions and remove context from prompt

--- utils/const.py
+++ utils/const.py
@@ -4,9 +4,15 @@ BOOTSTRAP_WRONG_CONTEXT_TASK = "bootstrap-wrong-context"
 FINETUNE_CRITIC_TASK = "finetune-critic"
 
 # Task-specific prompts
-INCORRECT_RESPONSE_INSTRUCTION = "You are now a hallucination generator. Please generate a hallucinated answer to the following question but adhere to the provided context. \
-    Ensure that your answer is different from the correct response supplied."
-INCORRECT_RESPONSE_TEMPLATE = "{}\n\n### CONTEXT\n{}\n\n### QUESTION:\n{}\n\n### CORRECT RESPONSE:\n{}\n\n### INCORRECT RESPONSE:\n"
+INCORRECT_RESPONSE_INSTRUCTION = (
+    "You are now a hallucination generator."
+    "Your task is to generate an incorrect but plausible answer to the given "
+    "question, given the correct answer. The incorrect answer should look "
+    "identical in formulation to the correct answer, but the core information "
+    "must be wrong to make it different from the correct answer provided. "
+    "You are allowed to arbitrarily chose the answer."
+)
+INCORRECT_RESPONSE_TEMPLATE = "{}\n\n### QUESTION:\n{}\n\n### CORRECT RESPONSE:\n{}\n\n### INCORRECT RESPONSE:\n"
 INCORRECT_RESPONSE_DELIMITER = "### INCORRECT RESPONSE:\n"
 
 SUMMARIZE_CONTEXT_INSTRUCTION = "Write a summary of this chunk of text that includes the main points and any important details."
