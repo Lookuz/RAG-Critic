1989b04e7cf01a042b64c1ee97ec265718fb8587
aboitreaud <aboitreaud@gmail.com>
2024-04-13 00:00:40 +0800

    No need model for answer eval task

--- main.py
+++ main.py
@@ -25,23 +25,24 @@ if __name__ == "__main__":
     else:
         raise AssertionError(f"Task {args.task} invalid!")
 
-    # Load model and tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(args.model_path)
+    if args.task != EVALUATE_ANSWERS_QUALITY_TASK:
+        # Load model and tokenizer
+        tokenizer = AutoTokenizer.from_pretrained(args.model_path)
 
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16,
-        bnb_4bit_use_double_quant=False,
-    )
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=False,
+        )
 
-    model = AutoModelForCausalLM.from_pretrained(
-        args.model_path,
-        quantization_config=bnb_config,
-        # device_map=args.device,
-        device_map="auto",
-        local_files_only = True
-    )
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model_path,
+            quantization_config=bnb_config,
+            # device_map=args.device,
+            device_map="auto",
+            local_files_only = True
+        )
 
     # Bootstrapping
     if args.task in [BOOTSTRAP_EVALUATION_GENERATION_TASK, BOOTSTRAP_INCORRECT_RESPONSE_TASK, GENERATE_RESPONSES_TASK]:            
