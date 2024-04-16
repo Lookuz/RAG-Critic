63840024b84aa8636723d4c7287e10cf1ff75d69
aboitreaud <aboitreaud@gmail.com>
2024-04-16 16:28:47 +0800

    style

--- scripts/evaluate_answer_quality.sh
+++ scripts/evaluate_answer_quality.sh
@@ -3,20 +3,18 @@ device="cuda"
 batch_size=16
 num_workers=8
 
-# Dataset parameters
+# Evaluation parameters
 dataset="triviaqa"
 model_path="meta-llama/Llama-2-7b-chat-hf"
-# model_path="/home/users/nus/e1101650/scratch/llama2-chat-hf-cache/models--meta-llama--Llama-2-7b-chat-hf/snapshots/09bd0f49e16738cdfaa6e615203e126038736eb0"
-data_path="triviaqa_datasets/TriviaQA/rc/qa/bootstrap/web-dev-refined-generated-response.json"
-metric="GLEU"
+data_path="triviaqa_datasets/TriviaQA/rc/qa/bootstrap/web-dev-refined-generated-response3.json"
+metric="GEval"
 
-save_data_path="triviaqa_datasets/answer_evaluation_results/{$metric}.json"
+save_data_path="triviaqa_datasets/answer_evaluation_results/$metric.json"
 save_every=10
 
 # Prompt the user for the authentication token
-echo -n "Enter your HuggingFace authentication token for using LLaMa 2 (leave empty if not using llama or loading from local): "
+echo -n "Enter your HuggingFace authentication token for using LLaMa 2: "
 read -s auth_token
-echo
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --task "evaluate_answers_quality" \
@@ -27,6 +25,6 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --num_workers $num_workers \
     --dataset $dataset \
     --data_path $data_path \
-    --hf_token $auth_token \
     --save_path $save_data_path \
-    --save_every $save_every
+    --save_every $save_every \
+    --hf_token $auth_token
