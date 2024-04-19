909948d08678635d25e2c7a9f070a714b7ab2dc5
aboitreaud <aboitreaud@gmail.com>
2024-04-19 10:02:05 +0800

    Add f1 score among metric choices

--- eval_answer_quality.py
+++ eval_answer_quality.py
@@ -8,6 +8,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 # from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 
 from triviaqa_datasets.datasets import ContextualizedQADatasetForQualityEvaluation, ContextualizedQADataLoader
+import nltk
+
+nltk.download('stopwords')
+nltk.download('punkt')
 
 
 # class DeepEvalLlama2_7B(DeepEvalBaseLLM):
@@ -52,7 +56,7 @@ def evaluate_answers_quality(
     device=None,
     *args, **kwargs
 ):
-    print("Calculating similarity scores...")
+
     dataset = ContextualizedQADatasetForQualityEvaluation.from_dataset(dataset=dataset, data_path=data_path)
     dataloader = ContextualizedQADataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
     if metric == 'GLEU':
@@ -61,6 +65,13 @@ def evaluate_answers_quality(
 
         def score(input_tuple):
             return sentence_gleu(input_tuple[0], input_tuple[1])
+
+    elif metric == 'F1':
+        model = None
+        prepare_input = prepare_F1_input
+
+        def score(input_tuple):
+            return score_F1(input_tuple[0], input_tuple[1])
 
     elif metric == 'SentenceSimilarity':
         model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
@@ -117,11 +128,20 @@ def evaluate_answers_quality(
             # Save additional examples to new data files
             with open(save_path, "w") as f:
                 json.dump(scores, f, indent=4)
+    with open(save_path, "w") as f:
+        json.dump(scores, f, indent=4)
 
 
 def prepare_GLEU_input(r_gt, r_zs, r_cr, q=None, model=None):
+    r_gt = remove_stopwords(r_gt)
+    r_zs = remove_stopwords(r_zs)
+    r_cr = remove_stopwords(r_cr)
     r_gt = r_gt.lower()
     return ([r_gt.split()], r_zs.lower().split()), ([r_gt.split()], r_cr.lower().split())
+
+
+def prepare_F1_input(r_gt, r_zs, r_cr, q=None, model=None):
+    return (r_gt, r_zs), (r_gt, r_cr)
 
 
 # def prepare_GEval_input(r_gt, r_zs, r_cr, q=None, model=None):
@@ -143,3 +163,41 @@ def prepare_SentenceSimilarity_input(r_gt, r_zs, r_cr, q=None, model=None):
     embedding_zs = model.encode(r_zs, convert_to_tensor=True)
     embedding_cr = model.encode(r_cr, convert_to_tensor=True)
     return (embedding_gt, embedding_zs), (embedding_gt, embedding_cr)
+
+
+def remove_stopwords(input_string):
+    # Tokenize the string
+    words = nltk.word_tokenize(input_string)
+
+    # Get the stopwords
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+
+    # Filter out stopwords
+    filtered_words = [word for word in words if word.casefold() not in stop_words]
+
+    # Join the words back into a single string
+    result = ' '.join(filtered_words)
+
+    return result
+
+
+def score_F1(ground_truth_answer, predicted_answer):
+    # Tokenize the predicted answer and ground-truth answer using NLTK
+    predicted_tokens = nltk.word_tokenize(predicted_answer.lower())
+    ground_truth_tokens = nltk.word_tokenize(ground_truth_answer.lower())
+
+    # Remove stop words from the predicted tokens using NLTK's English stop word list
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    predicted_tokens = [token for token in predicted_tokens if token not in stop_words and token.isalnum()]
+
+    if len(predicted_tokens) == 0:
+        f1_score = 0
+    else:
+        precision = len(set(predicted_tokens) & set(ground_truth_tokens)) / len(set(predicted_tokens))
+        recall = len(set(predicted_tokens) & set(ground_truth_tokens)) / len(set(ground_truth_tokens))
+        # Compute the F1 score as the harmonic mean of precision and recall
+        if precision + recall == 0:
+            f1_score = 0
+        else:
+            f1_score = 2 * (precision * recall) / (precision + recall)
+    return f1_score
